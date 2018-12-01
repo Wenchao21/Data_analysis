ff753496146916e08766f3566f7e046d5395b269
Wenchao21 <hewenchao21@163.com>
2018-12-01 18:09:00 +0800

    modified: mycsv.py, make it to pep8

--- Ancient_peotry_analysis/mycsv.py
+++ Ancient_peotry_analysis/mycsv.py
@@ -1,17 +1,12 @@
 import csv
-import json
-import jieba
-import jieba.analyse
+
 import redis
 
-#import redis sql
+import jieba.posseg
 
 pool = redis.ConnectionPool(host="127.0.0.1", port=6379)
 
-r = redis.Redis(connection_pool=pool,db=2)
-
-r.set('ttt', 'hewenchao')
-
+r = redis.Redis(connection_pool=pool, db=2)
 
 
 decade = []
@@ -19,11 +14,10 @@ title = []
 author_name = []
 with open('result.csv') as f:
     reader = csv.reader(f)
-    for wordsss in reader:
-#        print(wordsss[0])
-        decade.append(wordsss[0])
-        title.append(wordsss[2])
-        author_name.append(wordsss[3])
+    for wordCount in reader:
+        decade.append(wordCount[0])
+        title.append(wordCount[2])
+        author_name.append(wordCount[3])
 
 
 New_title = set(decade)
@@ -31,29 +25,30 @@ New_title.remove('author_decade')
 
 list22 = list(New_title)
 num = len(New_title)
-print("All decae: %s, total: %s"%(list22,num))
+print("All: %s, total: %s" % (list22, num))
 
-countsong = 0
+countSong = 0
 
 for i in range(0, len(New_title)-1):
     for j in range(0, len(decade)-1):
         if str(decade[j]) == str(list22[i]):
-            countsong = countsong + 1
-print(countsong)
-#        seg_list = jieba.cut(str(wordsss[0]), cut_all=False)
-#        print("/".join(seg_list))
-#        string1 = string1 + str("/".join(seg_list))
+            countSong = countSong + 1
 
-#cipin fenxi
+print(countSong)
+seg_list = jieba.cut(str(wordCount[0]), cut_all=False)
+print("/".join(seg_list))
+string1 = string1 + str("/".join(seg_list))
 
-#tags = jieba.analyse.extract_tags(string1, topK=100)
-#print(tags)
-#print(','.join(seg_list))
+# 词频分析
 
-#import jieba.posseg
-#words = jieba.posseg.cut(string1)
+tags = jieba.analyse.extract_tags(string1, topK=100)
+print(tags)
+print(','.join(seg_list))
 
 
-#for w in words:
-#    print(w.word)
-#    print(w.flag)
+words = jieba.posseg.cut(string1)
+
+
+for i in words:
+    print(i.word)
+    print(i.flag)
